b4170c7726ed3e5a246205476e2929829d8a74e3
Timothee Guerin <tiguerin@microsoft.com>
2024-04-26 07:48:03 -0700

    Test

---
+++ test.ts
@@ -0,0 +1,63 @@
+const crossSpawn = require("cross-spawn");
+const { spawn } = require("child_process");
+
+export function execAsync(
+  spawnFn: any,
+  cmd: string,
+  args: string[]
+): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const child = spawnFn(cmd, args);
+    let stdall = Buffer.from("");
+    let stdout = Buffer.from("");
+    let stderr = Buffer.from("");
+
+    if (child.stdout) {
+      child.stdout.on("data", (data) => {
+        stdout = Buffer.concat([stdout, data]);
+        stdall = Buffer.concat([stdall, data]);
+      });
+    }
+
+    if (child.stderr) {
+      child.stderr.on("data", (data) => {
+        stderr = Buffer.concat([stderr, data]);
+        stdall = Buffer.concat([stdall, data]);
+      });
+    }
+
+    child.on("error", (err) => {
+      reject(err);
+    });
+
+    child.on("close", (code) => {
+      resolve({ code, stdout, stderr, stdall });
+    });
+  });
+}
+
+try {
+  await execAsync(crossSpawn, "npm", ["ls"]);
+} catch (error) {
+  console.error("-----FAILED WITH CROSS SPAWN-----");
+  console.error(error);
+  console.error("-".repeat(50));
+}
+
+if (process.platform === "win32") {
+  try {
+    await execAsync(crossSpawn, "npm.cmd", ["ls"]);
+  } catch (error) {
+    console.error("-----FAILED WITH CROSS SPAWN .cmd-----");
+    console.error(error);
+    console.error("-".repeat(50));
+  }
+}
+
+try {
+  await execAsync(spawn, "npm", ["ls"]);
+} catch (error) {
+  console.error("-----FAILED WITH REGULAR SPAWN-----");
+  console.error(error);
+  console.error("-".repeat(50));
+}
